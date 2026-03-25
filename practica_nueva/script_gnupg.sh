34788a57bcdf9078e2ea624bb50c4adcc02aeca8
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 02:55:33 +0000

    confirmar nivel de confianza

--- practica_nueva/script_gnupg.sh
+++ practica_nueva/script_gnupg.sh
@@ -9,3 +9,4 @@ gpg --output doc_no_cifrado2.txt --clearsign doc_no_cifrado2.txt #document with 
 gpg --verify ag_doc_no_cifrado_firmado.txt #verification
 gpg --edit-key FD999E623F0E6A05942CE911A91C2A6252985C72 # we give level of trust 
 gpg --sign-key A91C2A6252985C72 #We signed the key to ratify
+gpg --verify ag_doc_no_cifrado_firmado.txt #check confidence level
