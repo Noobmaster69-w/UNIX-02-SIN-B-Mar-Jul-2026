80f40065a4acec205f0e0bd452da3c913391b850
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 02:03:22 +0000

    escribir el mensaje

--- practica_nueva/script_gnupg.sh
+++ practica_nueva/script_gnupg.sh
@@ -1,3 +1,4 @@
 gpg --full-generate-key #genera llave publica
 gpg --armor --export #export public key
- gpg --import  ag_llave_publica.asc # importar llave
+gpg --import  ag_llave_publica.asc # import public key
+echo "burrito pizza?" > doc_no_cifrado2.txt # write the message
