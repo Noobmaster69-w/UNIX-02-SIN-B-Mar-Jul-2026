620ca86d686eff7424fb1496d371caf028f5b1ab
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 02:52:53 +0000

    firmamos la clave

--- practica_nueva/script_gnupg.sh
+++ practica_nueva/script_gnupg.sh
@@ -8,3 +8,4 @@ gpg --decrypt ag_doc_cifrado.txt #We decrypted the message
 gpg --output doc_no_cifrado2.txt --clearsign doc_no_cifrado2.txt #document with signature
 gpg --verify ag_doc_no_cifrado_firmado.txt #verification
 gpg --edit-key FD999E623F0E6A05942CE911A91C2A6252985C72 # we give level of trust 
+gpg --sign-key A91C2A6252985C72 #We signed the key to ratify
