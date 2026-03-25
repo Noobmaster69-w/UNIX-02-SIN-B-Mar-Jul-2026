908b2319fab353c9d29343cd7a42ac7207fe2708
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 02:06:45 +0000

    comprobar el mensaje

--- practica_nueva/script_gnupg.sh
+++ practica_nueva/script_gnupg.sh
@@ -2,3 +2,4 @@ gpg --full-generate-key #genera llave publica
 gpg --armor --export #export public key
 gpg --import  ag_llave_publica.asc # import public key
 echo "burrito pizza?" > doc_no_cifrado2.txt # write the message
+cat doc_no_cifrado2.txt # to check the message
