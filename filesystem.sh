9a86b42bd40888e19fc45956c2342be1ad3f8355
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 13:47:49 +0000

    echo_stat

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,5 @@ pwd #imprime el directorio en el que estoy trabajando
 cd #change dicetory
 ls -F #list the contents to the current working directory
 ls -i #list files
+echo "Hola" > test.txt # create a new file and write a text
+stat test.txt #provides detailed information about the file
