81429ea19412fd5d15e50c9a32ca6ba0371a60a5
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 02:58:47 +0000

    frimamos en formato binario

--- practica_nueva/script_gnupg.sh
+++ practica_nueva/script_gnupg.sh
@@ -10,3 +10,4 @@ gpg --verify ag_doc_no_cifrado_firmado.txt #verification
 gpg --edit-key FD999E623F0E6A05942CE911A91C2A6252985C72 # we give level of trust 
 gpg --sign-key A91C2A6252985C72 #We signed the key to ratify
 gpg --verify ag_doc_no_cifrado_firmado.txt #check confidence level
+gpg --output doc_no_firmado_binario.txt --sign doc_no_cifrado2.txt #We signed the document in binary format.
