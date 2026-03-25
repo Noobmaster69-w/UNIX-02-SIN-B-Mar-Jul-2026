c9e6a49116e4526f6b33d85847f677bcfd881242
Bryan_david <brmerinoji@uide.edu.ec>
2026-03-25 02:31:42 +0000

    documento con firma

--- practica_nueva/script_gnupg.sh
+++ practica_nueva/script_gnupg.sh
@@ -5,3 +5,4 @@ echo "burrito pizza?" > doc_no_cifrado2.txt # write the message
 cat doc_no_cifrado2.txt # to check the message
 gpg --output doc_cifrado.txt --encrypt --recipient FD999E623F0E6A05942CE911A91C2A6252985C72 #we encrypt
 gpg --decrypt ag_doc_cifrado.txt #We decrypted the message
+gpg --output doc_no_cifrado2.txt --clearsign doc_no_cifrado2.txt #document with signature
