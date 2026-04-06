ead16910968cc5ea38f4be1efec29c88da07cb30
Bryan_david <brmerinoji@uide.edu.ec>
2026-04-06 12:44:24 +0000

    directory proyecto

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,9 @@ cd #change directory
 cd /home/codespace #Move directly to the main folder
 cd ~ #go to home directory
 cd $HOME #Save the path to your home folder in a variable called HOME
+mkdir proyecto #Create a directory called "proyecto"
+cd proyecto/ #enter the directory called project
+ls -lai #List all files and folders with detailed information
+total 12
+925546 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:37 .
+918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:37 ..
