14fd70e658a9c2a3a0880120eb2783698807e257
Bryan_david <brmerinoji@uide.edu.ec>
2026-04-08 13:59:32 +0000

    Compilado

--- script_boot.sh
+++ script_boot.sh
@@ -22,3 +22,16 @@ ls -it #List files showing their inode number and sorted by modification date
 ls / #ls absolute route
 ls /etc | head -20 #displays the first 20 files in the directory
 ls /dev | head -20 #It contains files that represent devices
+
+sudo apt install -y git vim make gcc libncurses-dev flex bison bc \ cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
+
+gcc , make — #kernel compilation and BusyBox
+libncurses-dev — #menús interactivos de configuración ( menuconfig )
+flex , bison , bc — #required by the kernel build process
+cpio  #to create the initramfs
+libelf-dev , libssl-dev — #kernel dependencies
+syslinux — #the bootloader
+dosfstools — #to create the FAT filesystem
+qemu-system-x86 — #to test the image without the need for real hardware
+
+make -j 2 #It allows you to run the compilation process using 2 tasks in parallel to make it faster.
