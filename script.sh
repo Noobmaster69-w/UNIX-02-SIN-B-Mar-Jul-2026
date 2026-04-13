b6c8980000db11a89b9b4ced6a3a5cdeff468f6a
Bryan_david <brmerinoji@uide.edu.ec>
2026-04-13 12:57:05 +0000

    rm -rf

--- script.sh
+++ script.sh
@@ -4,3 +4,10 @@ ls --all #more descriptive form
 ls #List visible files
 - , -- #long option , short option
 a, all #everything
+ls -l #shows long format files
+ls -a #show all files
+ls -h #human readable
+ls -lah #combination of three commonly used options
+rm -rf #forcibly delete files
+mkdir -- -rf #create a folder called -rf, using -- to indicate that -rf is a name and not an option
+rmdir -- -rf #Delete a folder named -rf, using -- to indicate that -rf is a name and not an option
