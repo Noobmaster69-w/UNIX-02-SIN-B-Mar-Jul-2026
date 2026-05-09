e4762742b825f6aeb84a3cc4cecf9b054d023f56
Bryan_david <brmerinoji@uide.edu.ec>
2026-05-09 19:14:08 +0000

    2.2

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,3 +2,10 @@ ls # List files and directories in the current location
 LS # this gives us an error
 ls Documents # Show the contents of the Documents directory
 aptitude moo # Display the hidden cow message from aptitude
+ls -l # Show files and directories with detailed information
+ls -r # List files and directories in reverse order
+ls -l -r # Show detailed file information in reverse order
+ls -rl # Show detailed file information in reverse order
+ls -lr # Show detailed file information in reverse order
+aptitude -v moo # Show the aptitude cow message with extra verbosity
+
