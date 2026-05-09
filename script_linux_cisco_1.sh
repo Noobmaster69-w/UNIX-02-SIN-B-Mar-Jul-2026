a0ec8b50237421b34cc6e4594ec4430bed90bd4d
Bryan_david <brmerinoji@uide.edu.ec>
2026-05-09 19:05:00 +0000

    2.1

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,2 +1,4 @@
 ls # List files and directories in the current location
 LS # this gives us an error
+ls Documents # Show the contents of the Documents directory
+aptitude moo # Display the hidden cow message from aptitude
