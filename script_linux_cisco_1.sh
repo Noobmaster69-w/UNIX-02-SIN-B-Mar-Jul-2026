2fb0a26059448010925164f6d3a0e8d89194f101
Bryan_david <brmerinoji@uide.edu.ec>
2026-05-09 19:37:10 +0000

    3 and 4

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -8,4 +8,13 @@ ls -l -r # Show detailed file information in reverse order
 ls -rl # Show detailed file information in reverse order
 ls -lr # Show detailed file information in reverse order
 aptitude -v moo # Show the aptitude cow message with extra verbosity
+pwd # Display the current working directory
+cd Documents # Change to the Documents directory
+cd / # Change to the root directory
+cd /home/sysadmin # Change to the sysadmin user's home directory
+cd Documents # Change to the Documents directory
+cd School/Art # Change to the Art directory inside School
+cd .. # Move to the parent directory
+cd . # Stay in the current directory
+cd ~ # Change to the current user's home directory
 
