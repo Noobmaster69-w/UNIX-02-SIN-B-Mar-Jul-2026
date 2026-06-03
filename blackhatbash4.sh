4f184feefb2f22e15bc4206f99c42834252b3a47
Bryan_david <brmerinoji@uide.edu.ec>
2026-06-03 12:30:51 +0000

    double parentheses

--- blackhatbash4.sh
+++ blackhatbash4.sh
@@ -1,9 +1,15 @@
 #!/bin/bash/env bash
 
+set-x
 
 let result="4 * 5"
-echo ${result}
+echo ${result1}
 
 
 let result=(("5 * 5"))
-echo ${result1}
+echo ${result2}
+
+result=$(expr 5 + 505)
+echo ${result3}
+
+set+x
