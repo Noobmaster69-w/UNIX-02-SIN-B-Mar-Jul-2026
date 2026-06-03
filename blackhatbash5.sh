616a6989948c19c8ae1e7f0feae418d9de7ea288
Bryan_david <brmerinoji@uide.edu.ec>
2026-06-03 13:34:39 +0000

    force error

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,8 +1,25 @@
 #!/bin/bash/env bash
-set -xr
+set -x
 
 touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl fallo"
+
+echo "uno" > archivo.txt
+cat archivo.txt
+echo "uno-uno" > archivo.txt
+cat archivo.txt
+echo "dos-dos" > archivo.txt
+cat archivo.txt
+echo "tres" > archivo.txt
+cat archivo.txt
+echo "tres" >> archivo.txt
+cat archivo.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+lzl 2> error.txt
+cat error.txt
+
 set +x
