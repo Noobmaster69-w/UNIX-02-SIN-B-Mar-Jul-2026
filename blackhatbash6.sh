42b4db50d052e569e2a7ddf768f242ac05bf56a1
Bryan_david <brmerinoji@uide.edu.ec>
2026-06-08 12:57:58 +0000

    ping variables

--- blackhatbash6.sh
+++ blackhatbash6.sh
@@ -2,8 +2,13 @@
 ls -l / | grep "bin"
 SCRIPT_NAME="${0}"
 TARGET="${1}"
+
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
 ping -c 3 "${TARGET}"
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
+
+for args in "$@"; do
+    echo "${args}"
+done
