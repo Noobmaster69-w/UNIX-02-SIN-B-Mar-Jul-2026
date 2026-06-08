5ffab177f8aebcd228edfe7819f8a524804152b3
Bryan_david <brmerinoji@uide.edu.ec>
2026-06-08 13:15:28 +0000

    exit codes

--- input_prompting.sh
+++ input_prompting.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-
 echo "What is your first name?"
 read -r firstname
+
 echo "What is your last name?"
 read -r lastname
+
 echo "Your first name is ${firstname} and your last name is
 ${lastname}"
+
+ls -l > /dev/null
+echo "The exit code of the ls command was: $?"
+
+lzl 2> /dev/null
+echo "The exit code of the non-existing lzl command was: $?"
+
