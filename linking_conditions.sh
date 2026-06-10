8f06ba66e433a0e01271e23f3edfd2138e1daacf
Bryan_david <brmerinoji@uide.edu.ec>
2026-06-10 12:48:33 +0000

    OR_condition

--- linking_conditions.sh
+++ linking_conditions.sh
@@ -5,4 +5,8 @@ if [[ -f "file.txt" ]] && [[ -s "file.txt" ]]; then
     echo "The file exists and its size is greater than zero."
 fi
 
-
+DIR_NAME="dir_test"
+mkdir "${DIR_NAME}"
+if [[ -f "${DIR_NAME}" ]] || [[ -d "${DIR_NAME}" ]]; then
+    echo "${DIR_NAME} is either a file or a directory."
+fi
