0df52b558a0903373eee07cac443a5ac01721aaf
Bryan_david <brmerinoji@uide.edu.ec>
2026-06-17 12:44:08 +0000

    log.txt

---
+++ blackhatbash8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/
+
+awk '{print $1}' log.txt
+
+awk '{print $1,$2,$3}' log.txt # We tested each one, 1, 2 and 3
+
+awk '{print $1,$NF}' log.txt # We tested just $NF
+
+awk -F',' '{print $1}' example_csv.txt # We added $2
+
+awk 'NR < 10' log.txt
+
+
+
+
+
+
